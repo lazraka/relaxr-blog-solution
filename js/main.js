485361951ca5003b1d305508089b670ee03a1274
Amina Lazrak <amina.lazrak@gmail.com>
2018-03-18 12:51:03 -0400

    edit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(".link-bottom").on("click", function(){
+	alert("We are not currently taking sign ups");
+	$(".link-bottom").text("No sign Ups");
+
+});
+var slideOnRead = false;
+
+$(".readmore").on("click", function(){
+	if (slideOnRead === false){
+		slideOnRead=true;
+		$(".readmore").html("Read Less");
+		$(".extra-read").slideDown();
+	}
+	else if (slideOnRead === true){
+		slideOnRead=false;
+		$(".extra-read").slideUp();
+		$(".readmore").html("Read More")
+	};
+});
+
+var slideOnLearn = false;
+
+$(".learnmore").on("click", function(){
+	if (slideOnLearn === false){
+		slideOnLearn=true;
+		$(".extra-learn").slideDown();
+		$(".learnmore").html("Learn Less");
+	}
+	else if (slideOnLearn === true){
+		slideOnLearn=false;
+		$(".extra-learn").slideUp();
+		$(".learnmore").html("Learn More")
+	};
+});
